da1182cbf8f88701e61aac092421a22db3d40481
Bear-a1t <152343852+Bear-a1t@users.noreply.github.com>
2026-01-24 02:07:29 +0000

    fixed i hope

--- g/dmw/webapp/index_stripped.js
+++ g/dmw/webapp/index_stripped.js
@@ -20,7 +20,7 @@ Module["expectedDataFileDownloads"]++;
       PACKAGE_PATH = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf("/")) + "/")
     }
     var PACKAGE_NAME = "index_stripped.data";
-    var REMOTE_PACKAGE_BASE = "/g/dmw/index_stripped.data";
+    var REMOTE_PACKAGE_BASE = "../g/dmw/index_stripped.data";
     if (typeof Module["locateFilePackage"] === "function" && !Module["locateFile"]) {
       Module["locateFile"] = Module["locateFilePackage"];
       err("warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)")
@@ -1241,7 +1241,7 @@ Module["expectedDataFileDownloads"]++;
 })();
 var moduleOverrides = Object.assign({}, Module);
 var arguments_ = [];
-Module["locateFile"] = (path) => "/g/dmw" + path;
+Module["locateFile"] = (path) => "../g/dmw/" + path;
 var thisProgram = "./this.program";
 var quit_ = (status, toThrow) => {
   throw toThrow
